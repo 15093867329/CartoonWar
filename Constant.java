e9abfd5848a33f99171104f7c18cb38d9ba200f1
15093867329 <2495489706@qq.com>
2019-08-24 16:28:02 +0800

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,38 @@
+package com.neuedu.constant;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+* @ClassName: Constant
+* @Description: 常量类
+* @author neuedu_ZLC
+* @date 2019年8月19日 下午12:56:00
+* @version 0.1
+*
+*/
+public class Constant {
+	
+	// 读取配置文件
+	public static Properties prop = new Properties();
+	static Integer Game_Width = null;
+	static Integer Game_Height = null;
+	static {
+		InputStream inStream = Constant.class.getResourceAsStream("/gameConfiguration.propertise");
+		try {
+			prop.load(inStream);
+			Game_Width = Integer.parseInt(prop.getProperty("Game_Width"));
+			Game_Height = Integer.parseInt(prop.getProperty("Game_Height"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	// 定义宽度
+	public static final int GAME_WIDTH = Game_Width;
+	// 定义高度
+	public static final int GAME_HEIGHT = Game_Height;
+	
+	
+
+}
