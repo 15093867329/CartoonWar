c31ef472a268ebde788569a66230f97e691240c8
15093867329 <2495489706@qq.com>
2019-08-24 16:29:10 +0800

    Create GameObj.java

---
+++ GameObj.java
@@ -0,0 +1,21 @@
+package com.neuedu.entity;
+
+import java.awt.Image;
+
+/**
+* @ClassName: GameObj
+* @Description: 所有游戏角色的父类
+* @author neuedu_ZLC
+* @date 2019年8月20日
+* @version 0.1
+*
+*/
+public abstract class GameObj {
+	// X轴
+	public int x;
+	// Y轴
+	public int y;
+	// 图片
+	public Image img;
+
+}
