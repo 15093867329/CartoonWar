225ed778f5cf06ad72465d72bb4894bf71cbe8cb
15093867329 <2495489706@qq.com>
2019-08-24 16:30:24 +0800

    Create Prop.java

---
+++ Prop.java
@@ -0,0 +1,52 @@
+package com.neuedu.entity;
+
+import java.awt.Graphics;
+
+import com.neuedu.action.ActionAble;
+import com.neuedu.constant.Constant;
+import com.neuedu.util.GetImageUtil;
+
+/**
+* @ClassName: Prop
+* @Description: 道具类
+* @author neuedu_ZLC
+* @date 2019年8月23日
+* @version .01
+*
+*/
+public class Prop extends GameObj implements ActionAble {
+	public Prop() {
+		// TODO Auto-generated constructor stub
+	}
+	private int speed;
+	
+	public Prop(int x,int y,String imgName) {
+		this.x = x;
+		this.y = y;
+		this.img = GetImageUtil.geyImg(imgName);
+		this.speed = 5;
+	}
+	
+	
+	
+	
+    private double theta = Math.PI/4;
+	@Override
+	public void move() {
+		x += speed*Math.cos(theta);
+		x += speed*Math.sin(theta);
+		if(x<0 || x>Constant.GAME_WIDTH-img.getWidth(null)) {
+			theta = Math.PI-theta;
+		}
+		if(y<35 || y>Constant.GAME_HEIGHT-img.getHeight(null)) {
+			theta = -theta;
+		}
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		g.drawImage(img, x, y, null);
+		
+	}
+
+}
