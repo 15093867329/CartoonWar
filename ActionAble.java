ccdaa48f6a8a8eaa6b1cb9e15cb093a862f2166b
15093867329 <2495489706@qq.com>
2019-08-24 16:25:54 +0800

    Create ActionAble.java

---
+++ ActionAble.java
@@ -0,0 +1,19 @@
+package com.neuedu.action;
+
+import java.awt.Graphics;
+
+/**
+* @ClassName: ActionAble
+* @Description: 行为接口
+* @author neuedu_ZLC
+* @date 2019年8月20日
+* @version 0.1
+*
+*/
+public interface ActionAble {
+	// 移动方法
+	void move();
+	// 画方法
+	void draw(Graphics g);
+
+}
