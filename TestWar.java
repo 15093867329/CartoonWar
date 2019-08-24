863e9c122110eb8c4e24a2f6f494b09dccdbd210
15093867329 <2495489706@qq.com>
2019-08-24 16:30:44 +0800

    Create TestWar.java

---
+++ TestWar.java
@@ -0,0 +1,18 @@
+package com.neuedu.test;
+
+import com.neuedu.client.GameClient;
+
+/**
+* @ClassName: TestWar
+* @Description: 测试类
+* @author neuedu_ZLC
+* @date 2019年8月19日 下午1:09:49
+* @version 0.1
+*
+*/
+public class TestWar {
+	public static void main(String[] args) {
+		new GameClient().launchFrame();
+	}
+
+}
