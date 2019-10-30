40f917a37a349970f80edc1ea778f84477efedca
15093867329 <2495489706@qq.com>
2019-10-30 10:50:25 +0800

    Create SqlSessionFacUtil (2).java

---
+++ SqlSessionFacUtil (2).java
@@ -0,0 +1,23 @@
+package com.neuedu.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SqlSessionFacUtil {
+    private static SqlSessionFactory  sqlSessionFactory= null;
+    public static SqlSessionFactory createSqlSessionFactory(){
+        String resource = "SqlMapConifg.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        return sqlSessionFactory;
+    }
+}
