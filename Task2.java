115bdd8b62841121640b672fd0d3b9e99bf8be43
15093867329 <2495489706@qq.com>
2019-10-30 11:02:43 +0800

    Create Task2.java

---
+++ Task2.java
@@ -0,0 +1,8 @@
+public class Task2{
+	public static void main(String[] args){
+		int num = 19;
+		System.out.print(Integer.toBinaryString(num));
+		System.out.print(Integer.toHexString(num));
+		System.out.print(Integer.toOctalString(num));
+	}
+}
