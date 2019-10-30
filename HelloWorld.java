5afcc4a251ac8a8782e70ba3ab318edadb59243a
15093867329 <2495489706@qq.com>
2019-10-30 11:00:42 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class HelloWorld{
+    public static void main(String[] args){
+        Random random = new Random();
+		int num = random.nextInt(60);
+		System.out.print(num);
+	}
+}
