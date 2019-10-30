f46df4afa396a20975253d38450eb8e050ce059b
15093867329 <2495489706@qq.com>
2019-10-30 11:02:51 +0800

    Create Task3.java

---
+++ Task3.java
@@ -0,0 +1,10 @@
+public class Task3{
+	public static void main(String[] args){
+	    long Num = 12345678901L;
+		System.out.println(Num);
+		float Num1 = 3.1415926F;
+		System.out.println(Num1);
+		double Num2 = 1234567890.0987654321;
+		System.out.println(Num2);
+	}
+}
