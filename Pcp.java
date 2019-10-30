cb1b9270fdcf361de007d28bea1aa7d70f55c7cc
15093867329 <2495489706@qq.com>
2019-10-30 11:01:20 +0800

    Create Pcp.java

---
+++ Pcp.java
@@ -0,0 +1,17 @@
+// ¼ÓºÅ
+public class Pcp{
+	public static void main(String[] args){
+		// System.out.print(getSum(10,50));
+		int a = 1;
+	    a = ++a;
+		a = a++;
+		a = a++ + ++a;
+	    // System.out.print(a);
+		System.out.print(a-- + ++a + a++ + a);
+	}
+	// public static int getSum(int num1,int num2){
+		// int result = num1+num2;
+		// return result;
+	// }
+	
+}
