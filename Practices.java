942b48c417bcbd22e665e3914111718270bb1c70
15093867329 <2495489706@qq.com>
2019-10-30 11:01:28 +0800

    Create Practices.java

---
+++ Practices.java
@@ -0,0 +1,14 @@
+// ¼ÓºÅ
+public class Pcp{
+	public static void main(String[] args){
+		// System.out.print(getSum(10,50));
+		int X = 3;
+	    int Y = X++ + ++X +X*10;
+	    System.out.print(X)
+	}
+	// public static int getSum(int num1,int num2){
+		// int result = num1+num2;
+		// return result;
+	// }
+	
+}
