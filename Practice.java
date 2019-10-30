814314fa2602ef7048550727ee3dc6979a5310bd
15093867329 <2495489706@qq.com>
2019-10-30 11:01:24 +0800

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,9 @@
+public class Parctices{
+	public static void main(String[] args){
+		System.out.print(getSum(10,50));
+	}
+	public static int getSum(int num1,int num2){
+		int result = num1+num2;
+		return result;
+	}
+}
