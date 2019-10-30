c86a14dee09248100722e9794d3a913ee61c044a
15093867329 <2495489706@qq.com>
2019-10-30 10:50:55 +0800

    Create UserDao (2).java

---
+++ UserDao (2).java
@@ -0,0 +1,14 @@
+package com.neuedu.dao;
+
+import com.neuedu.entity.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    // 查询用户
+    List<User> getAllUser();
+
+    // 判断用户名密码是否存在
+    User queryUserIsExists(User user);
+}
