6d6f3c4bff748b2e32a4ea6531905a412eca4f2e
15093867329 <2495489706@qq.com>
2019-10-30 10:51:26 +0800

    Create UserServiceImpl (2).java

---
+++ UserServiceImpl (2).java
@@ -0,0 +1,21 @@
+package com.neuedu.service;
+
+import com.neuedu.dao.UserDao;
+import com.neuedu.entity.User;
+import com.neuedu.utils.SqlSessionFacUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+public class UserServiceImpl implements UserService{
+
+    private SqlSessionFactory sqlSessionFactory = SqlSessionFacUtil.createSqlSessionFactory();
+
+    @Override
+    public User login(User user) {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        User user1 = mapper.queryUserIsExists(user);
+        sqlSession.close();
+        return user1;
+    }
+}
