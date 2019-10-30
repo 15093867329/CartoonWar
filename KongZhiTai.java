e9215ec66639e94047632a35ef9dbc083eea59d5
15093867329 <2495489706@qq.com>
2019-10-30 11:01:07 +0800

    Create KongZhiTai.java

---
+++ KongZhiTai.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class KongZhiTai{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("请输入用户名:");
+		String name = scanner.next();
+		System.out.print("您的用户名是:"+name);
+	}
+}
