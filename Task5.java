4a500fa577d712a065bcbc78a7cb86683e314210
15093867329 <2495489706@qq.com>
2019-10-30 11:03:04 +0800

    Create Task5.java

---
+++ Task5.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Task5{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		int Num = scanner.nextInt();
+		System.out.print(Num*Num);
+	}
+}
